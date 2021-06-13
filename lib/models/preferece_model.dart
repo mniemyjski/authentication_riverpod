115b98945cad07f27c1f3e4c075fc4a147848988
Antek512 <niemyjski.marcel@gmail.com>
2021-06-13 19:45:17 +0200

    commit;

--- lib/models/preferece_model.dart
+++ lib/models/preferece_model.dart
@@ -2,26 +2,26 @@ import 'package:equatable/equatable.dart';
 import 'package:flutter/cupertino.dart';
 
 class Preference extends Equatable {
-  final bool init;
+  final String uid;
 
-  Preference({required this.init});
+  Preference({required this.uid});
 
   @override
   bool get stringify => true;
 
   @override
-  List<Object?> get props => [init];
+  List<Object?> get props => [uid];
 
   factory Preference.fromMap(Map<String, dynamic> map) {
     return new Preference(
-      init: map['init'] as bool,
+      uid: map['uid'] as String,
     );
   }
 
   Map<String, dynamic> toMap() {
     // ignore: unnecessary_cast
     return {
-      'init': this.init,
+      'uid': this.uid,
     } as Map<String, dynamic>;
   }
 }
--- lib/widgets/custom_drawer/custom_drawer.dart
+++ lib/widgets/custom_drawer/custom_drawer.dart
@@ -4,7 +4,6 @@ import 'package:authentication_riverpod/screens/screens.dart';
 import 'package:authentication_riverpod/utilities/utilities.dart';
 import 'package:authentication_riverpod/widgets/custom_drawer/widget/header.dart';
 import 'package:authentication_riverpod/widgets/custom_drawer/widget/item.dart';
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
